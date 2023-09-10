1a25c78170969377e7beda2c2f621bb0aadb5d31
maticpunk <maticpunk@gmail.com>
2023-09-10 04:32:35 +0400

    first commit

---
+++ smart-contract/config/CollectionConfig.ts
@@ -0,0 +1,35 @@
+import CollectionConfigInterface from '../lib/CollectionConfigInterface';
+import * as Networks from '../lib/Networks';
+import * as Marketplaces from '../lib/Marketplaces';
+import whitelistAddresses from './whitelist.json';
+
+const CollectionConfig: CollectionConfigInterface = {
+  testnet: Networks.polygonTestnet,
+  mainnet: Networks.polygonMainnet,
+  // The contract name can be updated using the following command:
+  // yarn rename-contract NEW_CONTRACT_NAME
+  // Please DO NOT change it manually!
+  contractName: 'MPLC',
+  tokenName: 'My NFT Token',
+  tokenSymbol: 'MNT',
+  hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
+  maxSupply: 10000,
+  whitelistSale: {
+    price: 0.01,
+    maxMintAmountPerTx: 1,
+  },
+  preSale: {
+    price: 0.01,
+    maxMintAmountPerTx: 2,
+  },
+  publicSale: {
+    price: 0.01,
+    maxMintAmountPerTx: 5,
+  },
+  contractAddress: "0xE48e70f2E235B5f31F03b58cFDA000730f948CB7",
+  marketplaceIdentifier: 'my-nft-token',
+  marketplaceConfig: Marketplaces.openSea,
+  whitelistAddresses,
+};
+
+export default CollectionConfig;
